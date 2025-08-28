66f38712533e18b1e93814fda28f8e031fef11c5
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-28 16:10:55 -0700

    feat(products): add products collection and tRPC procedures
    - Implement products collection with fields for name, description, price, category, images and refund policy
    - Add tRPC router and procedures for fetching products by category
    - Update category pages to use client-side data fetching with tRPC
    - Include products collection in payload config and update types

---
+++ src/app/(app)/(home)/[category]/layout.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+
+import { trpc } from "@/trpc/server";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+interface LayoutProps {
+  params: {
+    category: string;
+  };
+}
+
+export default async function HomeLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ category: string }>;
+}> &
+  LayoutProps) {
+  const { category } = await params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({ category })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </HydrationBoundary>
+  );
+}
