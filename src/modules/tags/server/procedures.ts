b12ff7662a7f84e09dfa8bbaa9e97c02d2772707
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-30 22:35:25 +0500

    feat(Tags): add tagsRouter to handle tag-related queries

---
+++ src/modules/tags/server/procedures.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+
+export const tagsRouter = createTRPCRouter({
+  getMany: baseProcedure
+    .input(
+      z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(8),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { docs } = await ctx.payload.find({
+        collection: "tags",
+        page: input.cursor,
+        limit: input.limit,
+      });
+
+      return docs;
+    }),
+});
