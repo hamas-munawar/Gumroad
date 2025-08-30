1507f3842f8cd739a11d5fd1966f18c0a3dad2fa
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-30 17:32:03 +0500

    refactor(ProductFilters): improve filter button visibility logic by moving hasAnyFilters check

--- src/app/(app)/(home)/[category]/ProductFilters.tsx
+++ src/app/(app)/(home)/[category]/ProductFilters.tsx
@@ -6,16 +6,25 @@ import PriceFilter from './PriceFilter';
 const ProductFilters = () => {
   const [priceFilter, setPriceFilter] = usePriceFilter();
 
+  const hasAnyFilters = Object.entries(priceFilter).some(([, value]) => {
+    if (typeof value === "string") {
+      return value !== "";
+    }
+    return value !== null;
+  });
+
   return (
     <div>
       <div className="flex justify-between border-b p-4 cursor-default">
         <h4 className="text-xl font-medium">Filters</h4>
-        <button
-          className="underline text-lg cursor-pointer"
-          onClick={() => setPriceFilter(null)}
-        >
-          Clear
-        </button>
+        {hasAnyFilters && (
+          <button
+            className="underline text-lg cursor-pointer"
+            onClick={() => setPriceFilter(null)}
+          >
+            Clear
+          </button>
+        )}
       </div>
       <div>
         <PriceFilter />
