9301f59f8fab404bb660c1e836a3da9f549e5aea
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-31 07:25:57 +0500

    feat(TagsFilter): implement infinite query fetching and dynamic tag loading in TagsFilter component in Ui

--- src/app/(app)/(home)/[category]/TagsFilter.tsx
+++ src/app/(app)/(home)/[category]/TagsFilter.tsx
@@ -4,8 +4,25 @@ import { ChevronRight } from 'lucide-react';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import { Label } from '@/components/ui/label';
+import { useTRPC } from '@/trpc/client';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 const TagsFilter = () => {
+  const trpc = useTRPC();
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      trpc.tags.getMany.infiniteQueryOptions(
+        {
+          limit: 2,
+        },
+        {
+          getNextPageParam: (lastPage) => {
+            return lastPage.docs.length > 0 ? lastPage.nextPage : undefined;
+          },
+        }
+      )
+    );
+
   return (
     <Collapsible className="rounded-md overflow-hidden w-full">
       <CollapsibleTrigger className="w-full cursor-pointer group flex items-center justify-between text-lg font-medium p-4">
@@ -13,12 +30,26 @@ const TagsFilter = () => {
         <ChevronRight className="h-5 w-5 text-black transition-transform duration-200 group-data-[state=open]:rotate-90" />
       </CollapsibleTrigger>
       <CollapsibleContent className="px-4 pb-4 flex flex-col gap-2 mb-2">
-        <div className="flex items-center justify-between">
-          <Label htmlFor="checkbox" className="text-base">
-            Lorem
-          </Label>
-          <Checkbox id="checkbox" />
-        </div>
+        {data?.pages.map((page) =>
+          page.docs.map((tag) => (
+            <div className="flex items-center justify-between" key={tag.id}>
+              <Label htmlFor="checkbox" className="text-base">
+                {tag.name}
+              </Label>
+              W
+              <Checkbox id="checkbox" />
+            </div>
+          ))
+        )}
+        {hasNextPage && (
+          <button
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+            className="underline font-medium justify-start text-start disabled:opacity-50"
+          >
+            Load more...
+          </button>
+        )}
       </CollapsibleContent>
     </Collapsible>
   );
--- src/modules/tags/server/procedures.ts
+++ src/modules/tags/server/procedures.ts
@@ -7,11 +7,11 @@ export const tagsRouter = createTRPCRouter({
     .input(
       z.object({
         cursor: z.number().default(1),
-        limit: z.number().default(8),
+        limit: z.number().default(2),
       })
     )
     .query(async ({ ctx, input }) => {
-      const { docs } = await ctx.payload.find({
+      const docs = await ctx.payload.find({
         collection: "tags",
         page: input.cursor,
         limit: input.limit,
