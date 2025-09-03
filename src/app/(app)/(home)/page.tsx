f86bc580b89b9bb8d27c9a1a6b9e2e31fccea443
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-09-04 01:19:30 +0500

    feat: implement HomePage component with product filtering and listing functionality

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,49 @@
-export default function HomePage() {
-  return <section>HomePage</section>;
+import { SearchParams } from "nuqs";
+import { Suspense } from "react";
+
+import { DEFAULT_PRODUCTS_LIMIT } from "@/constants";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import ProductFilters from "./[...categories]/ProductFilters";
+import ProductsList, {
+  ProductsListSkeleton,
+} from "./[...categories]/ProductsList";
+import SortFilter from "./[...categories]/SortFilter";
+
+export default async function HomePage({
+  searchParams,
+}: Readonly<{
+  searchParams: Promise<SearchParams>;
+}>) {
+  const productFilters = await searchParams;
+
+  const queryClient = getQueryClient();
+  await queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...productFilters,
+      limit: DEFAULT_PRODUCTS_LIMIT,
+    })
+  );
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-10 p-6 gap-8">
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between col-span-full">
+        <div className="text-2xl font-medium">Curated for you</div>
+        <div>
+          <SortFilter />
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-2 border border-black rounded-md self-start">
+        <ProductFilters />
+      </div>
+      <div className="md:col-span-3 lg:col-span-8">
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <Suspense fallback={<ProductsListSkeleton />}>
+            <ProductsList />
+          </Suspense>
+        </HydrationBoundary>
+      </div>
+    </div>
+  );
 }
