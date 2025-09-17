495ed996bdffcd8beffa37216598b76752cb226e
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-09-17 13:34:26 +0500

    fix: remove unnecessary padding from TenantPage layout

--- src/app/(app)/(tenants)/store/[tenantSlug]/page.tsx
+++ src/app/(app)/(tenants)/store/[tenantSlug]/page.tsx
@@ -30,7 +30,7 @@ const TenantPage = async ({
     })
   );
   return (
-    <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-10 px-4 lg:px-20 py-6 gap-8 place-content-start">
+    <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-10  py-6 gap-8 place-content-start">
       <div className="flex flex-col lg:flex-row lg:items-center justify-between col-span-full">
         <div className="text-2xl font-medium">Curated for you</div>
         <div>
