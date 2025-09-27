0b5e35b8b51d60ef54b9f068e6885b35d93f6c5f
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-09-27 15:50:31 +0500

    refactor: comment out unused code in generateTenantSubdomain function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,22 +6,22 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantSubdomain(tenantSlug: string) {
-  const isDevelopmentMode = process.env.NODE_ENV === "development";
-  const isDomainRewriteFalse = process.env.DOMAIN_REWRITE === "false";
+  // const isDevelopmentMode = process.env.NODE_ENV === "development";
+  // const isDomainRewriteFalse = process.env.DOMAIN_REWRITE === "false";
 
-  // If in development or domain rewrite is explicitly set to false, use the main app URL with /store/tenantSlug
-  if (isDevelopmentMode || isDomainRewriteFalse) {
-    return `${process.env.NEXT_PUBLIC_APP_URL}/store/${tenantSlug}`;
-  }
-
-  const protocol = "https";
-  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN || "";
-
-  // if (process.env.NODE_ENV === "development") {
-  //   protocol = "http";
+  // // If in development or domain rewrite is explicitly set to false, use the main app URL with /store/tenantSlug
+  // if (isDevelopmentMode || isDomainRewriteFalse) {
+  return `${process.env.NEXT_PUBLIC_APP_URL}/store/${tenantSlug}`;
   // }
 
-  return `${protocol}://${tenantSlug}.${domain}`;
+  // const protocol = "https";
+  // const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN || "";
+
+  // // if (process.env.NODE_ENV === "development") {
+  // //   protocol = "http";
+  // // }
+
+  // return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(amount: string | number, currency: string) {
